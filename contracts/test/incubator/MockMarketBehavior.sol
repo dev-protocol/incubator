66dd8cfd3a4bced026c34d29636a488081732a64
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-10-29 14:56:22 +0900

    update by lint

--- contracts/test/incubator/MockMarketBehavior.sol
+++ contracts/test/incubator/MockMarketBehavior.sol
@@ -11,13 +11,13 @@ contract MockMarketBehavior is IMarketBehavior {
 	mapping(bytes32 => address) private metrics;
 
 	function authenticate(
-		address ,
+		address,
 		string calldata,
 		string calldata,
 		string calldata,
 		string calldata,
 		string calldata,
-		address ,
+		address,
 		address
 	)
 		external
@@ -30,34 +30,34 @@ contract MockMarketBehavior is IMarketBehavior {
 		auth[address(0)] = true;
 	}
 
-	function schema() external override view returns (string memory) {
-		return "[\"GitHub Repository (e.g, your/awesome-repos)\", \"Khaos Public Signature\"]";
+	function schema() external view override returns (string memory) {
+		return
+			'["GitHub Repository (e.g, your/awesome-repos)", "Khaos Public Signature"]';
 	}
 
 	function getId(address _metrics)
-        external
-        override
-        view
-        returns (string memory)
-    {
+		external
+		view
+		override
+		returns (string memory)
+	{
 		return repositories[_metrics];
 	}
 
 	function getMetrics(string memory _repository)
-        external
-        override
-        view
-        returns (address)
-    {
+		external
+		view
+		override
+		returns (address)
+	{
 		return metrics[createKey(_repository)];
 	}
 
-
 	function createKey(string memory _repository)
-        private
-        pure
-        returns (bytes32)
-    {
+		private
+		pure
+		returns (bytes32)
+	{
 		return keccak256(abi.encodePacked(_repository));
 	}
 }
