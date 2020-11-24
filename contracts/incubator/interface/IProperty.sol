902bffebaeea09e1dc76b548b6ed0a7f97f4d413
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-11-24 16:39:39 +0900

    delete unnecessary interface

--- contracts/incubator/interface/IProperty.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MPL-2.0
-
-pragma solidity 0.6.12;
-
-interface IProperty {
-	function author() external view returns (address);
-
-	function changeAuthor(address _nextAuthor) external;
-}
